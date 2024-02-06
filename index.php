d8ecfffca506f01170c66d424bb332fa4bc90a15
Enjagura <114489232+Enjagura@users.noreply.github.com>
2024-02-06 22:36:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,29 +4,17 @@
         <link rel="stylesheet" type="text/css" href="./style.css" />
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Game | v.0.0.1</title>
     </head>
     <body>
         <div class="grid-container">
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
+            <?php 
+                $element = "<div class='grid-item'></div>"; 
+                $count = 6;
+                for ($i = 0; $i < $count; $i++) {
+                    echo $element;
+                }
+            ?>
         </div>
     </body>
 </html>
